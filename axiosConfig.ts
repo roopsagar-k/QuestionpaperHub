bd653896fa102d1856a4e5d583aa839d7fb9c5c2
RoopsagarK <roopsagarudayar9854@gmail.com>
2024-07-06 08:51:01 +0530

    axios update

--- axiosConfig.ts
+++ axiosConfig.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000",
+  baseURL:
+    process.env.NEXT_PUBLIC_BASE_URL || "https://questionpaper-hub.vercel.app/",
 });
 
 export default axiosInstance;
